741efbe01e80777d08f408962141d9aa780ee60d
CodeX1991 <oghenewonaholy@gmail.com>
2023-11-26 10:48:29 -0800

    Add Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Config the ssh server to authenticates only through ssh keys
+include stdlib
+
+file_line {'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication'
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^IdentityFile'
+}
