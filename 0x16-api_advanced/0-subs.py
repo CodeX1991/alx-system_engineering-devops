e93d2037441df656bc5032e0ca7e492a26b71f0e
CodeX1991 <oghenewonaholy@gmail.com>
2024-02-02 11:31:33 -0800

    Add count it file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,7 @@ If not a valid subriddit, return 0
 """
 
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
@@ -14,18 +15,18 @@ def number_of_subscribers(subreddit):
     for a given subreddit
     """
     # Set the Default URL strings
-    home_page_url = "https://www.reddit.com"
-    api_uri = "{}/r/{}/about.json".format(home_page_url, subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     # Set an User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
+    user_agent = {"User-Agent": "python/requests"}
 
     # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=user_agent, allow_redirects=False)
-
+    res = requests.get(url, headers=user_agent, allow_redirects=False)
+        
     # Checks if the subreddit is invalid
     if res.status_code in [302, 404]:
         return 0
-
+        
     # Return total subscribers of the subreddit
-    return res.json().get('data').get('subscribers')
+    results = res.json().get("data")
+    return results.get("subscribers")
